868941b0221f391dbad8dc9df9d87f0735c895db
Sharkawy <dev.elbehery@gmail.com>
2025-08-08 05:47:47 +0300

    feat: initialize NestJS app with Swagger docs, CORS, and validation setup

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: configService.get('cors.origin'),
+    origin: configService.get('cors.origin') || true,
     credentials: true,
   });
 
